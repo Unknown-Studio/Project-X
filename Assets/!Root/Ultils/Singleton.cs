77eff4a1f060a1c9ad6ff6e473be26ebf8ab39f6
Dung Nguyen <dungnguyen.suhdo@gmail.com>
2023-12-22 10:58:18 +0700

    feat: add name space in file singleton

--- Assets/!Root/Ultils/Singleton.cs
+++ Assets/!Root/Ultils/Singleton.cs
@@ -1,52 +1,50 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace Suhdo
 {
-    protected static T _instance;
-
-    public static T Instance
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        protected static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        _instance = new GameObject(typeof(T).ToString()).AddComponent<T>();
+                        DontDestroyOnLoad(_instance.gameObject);
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public static bool IsActive
+        {
+            get { return _instance != null; }
+        }
+
+        protected virtual void Awake()
         {
             if (_instance == null)
             {
-                _instance = GameObject.FindObjectOfType<T>();
-
-                if (_instance == null)
-                {
-                    _instance = new GameObject(typeof(T).ToString()).AddComponent<T>();
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
             }
-
-            return _instance;
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
         }
-    }
 
-    public static bool IsActive
-    {
-        get
+        protected virtual void OnAwake()
         {
-            return _instance != null;
         }
-    }
-
-    protected virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (_instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    protected virtual void OnAwake()
-    {
-
     }
 }
