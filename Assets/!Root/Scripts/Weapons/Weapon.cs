15c1ec7a07bd345ed54281fa13661d4be0a5466b
Suhdo Nguyen <dungvipnr@gmail.com>
2024-02-18 22:16:47 +0700

    feat: weapon setup

--- Assets/!Root/Scripts/Weapons/Weapon.cs
+++ Assets/!Root/Scripts/Weapons/Weapon.cs
@@ -1,12 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace Suhdo.Weapons
 {
 	public class Weapon : MonoBehaviour
 	{
+		private Animator _anim;
+		private GameObject _baseGameObject;
+
+		private void Awake()
+		{
+			_baseGameObject = transform.Find("Base").gameObject;
+			_anim = _baseGameObject.GetComponent<Animator>();
+		}
+
 		public void Enter()
 		{
 			print($"{transform.name} enter");
+			
+			_anim.SetBool("active", true);
 		}
 	}
 }
