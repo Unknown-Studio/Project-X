824aee662ceadc0ecb53baadb7f44822f9bb301d
Suhdo Nguyen <dungvipnr@gmail.com>
2024-02-19 22:04:09 +0700

    feat: timer to reset attack animation number

--- Assets/!Root/Scripts/Weapons/Weapon.cs
+++ Assets/!Root/Scripts/Weapons/Weapon.cs
@@ -1,46 +1,71 @@
 using System;
-using Unity.VisualScripting;
+using Suhdo.Ultils;
 using UnityEngine;
 
 namespace Suhdo.Weapons
 {
 	public class Weapon : MonoBehaviour
 	{
+		[SerializeField] private int numberOfAttack;
+		[SerializeField] private float attackCounterResetCoolDown;
+		
 		public event Action OnExit;
+
+		public int CurrentAttackCounter
+		{
+			get => _currentAttackCounter;
+			private set => _currentAttackCounter = value >= numberOfAttack ? 0 : value;
+		}
 		
 		private Animator _anim;
 		private GameObject _baseGameObject;
 		private AnimationEventHandler _eventHandler;
+		private int _currentAttackCounter;
+
+		private Timer _attackCounterResetTimer;
 
 		private void Awake()
 		{
 			_baseGameObject = transform.Find("Base").gameObject;
 			_anim = _baseGameObject.GetComponent<Animator>();
 			_eventHandler = _baseGameObject.GetComponent<AnimationEventHandler>();
+			_attackCounterResetTimer = new Timer(attackCounterResetCoolDown);
 		}
 
 		private void OnEnable()
 		{
 			_eventHandler.Exit += Exit;
+			_attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
 		}
 
 		private void OnDisable()
 		{
 			_eventHandler.Exit -= Exit;
+			_attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
 		}
 
 		public void Enter()
 		{
 			print($"{transform.name} enter");
 			
+			_attackCounterResetTimer.StopTimer();
 			_anim.SetBool("active", true);
+			_anim.SetInteger("counter", CurrentAttackCounter);
+		}
+
+		private void Update()
+		{
+			_attackCounterResetTimer.Tick();
 		}
 
 		private void Exit()
 		{
+			CurrentAttackCounter++;
 			_anim.SetBool("active", false);
-			
 			OnExit?.Invoke();
+			_attackCounterResetTimer.StartTimer();
 		}
+
+		private void ResetAttackCounter() => CurrentAttackCounter = 0;
 	}
 }
